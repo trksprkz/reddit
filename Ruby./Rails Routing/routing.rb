c1e9728e90e9843d8ba73dea87072ababfb72630
Issac Davis <54684726+trksprkz@users.noreply.github.com>
2022-11-28 09:40:17 -0800

    Update routing.rb

--- Ruby./Rails Routing/routing.rb
+++ Ruby./Rails Routing/routing.rb
@@ -17,7 +17,7 @@
     # they land on the root url (example: http://google.com)
 
     #Example:
-      #  root to: "Google#index"
+      root to: "Google#index"
 
 
 
@@ -25,13 +25,13 @@
     # The 7 main ways to do something (with GET, POST, PUT, and DELETE) as discussed earlier
 
     # How to implement them in Rails:
-      # get "/posts", to: "posts#index"
-      # get "/posts/new", to: "posts#new"
-      # get "/posts/:id", to: "posts#show"
-      # post "/posts", to: "posts#create"  # usually a submitted form
-      # get "/posts/:id/edit", to: "posts#edit"
-      # put "/posts/:id", to: "posts#update" # usually a submitted form
-      # delete "/posts/:id", to: "posts#destroy"
+      get "/posts", to: "posts#index"
+      get "/posts/new", to: "posts#new"
+      get "/posts/:id", to: "posts#show"
+      post "/posts", to: "posts#create"  # usually a submitted form
+      get "/posts/:id/edit", to: "posts#edit"
+      put "/posts/:id", to: "posts#update" # usually a submitted form
+      delete "/posts/:id", to: "posts#destroy"
 
       #Each route is basically a Ruby method that matches that particular URL and HTTP verb
       # with the correct controller action.
@@ -51,7 +51,7 @@
   # RESTful Routes with Rails -
     # Rails is setup to use the 7 actions, so built-in is a helper method.
     #this allows all 7 to be written in one line instead...
-      # resources :posts      (written in the config/routes.rb)
+      resources :posts      #(written in the config/routes.rb)
 
       # typing -> rails routes into the terminal outputs all routes
 
@@ -59,9 +59,7 @@
     #method.
     # You'll need to supply text to show, and a URL to link it to:
       # Example:
-        # link_to 'Edit comment", edit_post_path(3)
+        link_to 'Edit comment", edit_post_path(3)
 
-
-    # Rails generates helper methods for all of the routes: _path, _url, path, url
 
 
