ce1df49d6dab57e8d216eb8c7216e689959b7c42
Issac Davis <trksprkz@gmail.com>
2022-12-01 12:32:35 -0600

    setup validation for user, and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: [minimum: 4, maximum: 50], uniqueness: true
+  validates :body, length: [minimum: 0, maximum: 200], allow_blank: true
+
+  validates :url, presence: true
+    validates :url, format: { with: URI.regexp }, if: 'url.present?'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: [minimum: 4, maximum: 14], uniqueness: true
+
+  validates :email, presence: true, uniqueness: true, with: EMAIL_REGEX
+    EMAIL_REGEX =^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$
+    def valid_email? email
+      email= EMAIL_REGEX
+    end
+
+  validates :password, presence: true, length: [minimuum: 6, maximum: 8] uniqueness: true
 end
