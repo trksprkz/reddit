b22bd6997fc19fb5a3a8d811fae1c0ac64b2fe23
Issac Davis <trksprkz@gmail.com>
2022-12-03 00:21:09 -0600

    adjusted post migration and validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: [minimum: 4, maximum: 50], unique: true
-  validates :body, length: [minimum: 0, maximum: 200], allow_blank: true
-
-  validates :url, presence: true
+  validate :title
+  validate :body
+  validate :url
     validates :url, format: { with: URI.regexp }, if: 'url.present?'
 end
--- db/migrate/20221201180853_create_posts.rb
+++ db/migrate/20221201180853_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.string :title, presence :true, limit: 50, unique: true
+      t.text :body, limit: 200, allow_blank
       t.string :author_username
-      t.string :link
+      t.string :url
 
       t.timestamps
     end
